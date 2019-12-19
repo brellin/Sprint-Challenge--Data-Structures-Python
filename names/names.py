0d5c4133c9cd5cafd16391fb72e00241bd91b42b
brellin <cekakid@gmail.com>
2019-12-19 14:05:02 -0600

    names stretch

--- names/names.py
+++ names/names.py
@@ -1,4 +1,3 @@
-import itertools
 import time
 from binary_search_tree import BinarySearchTree
 
@@ -13,24 +12,18 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 one = BinarySearchTree(names_1[0])
-duplicates = []
+duplicates = set(names_1) & set(names_2)
 # for name_1 in names_1:
 #     one.insert(name_1)
 # for name_2 in names_2:
 #     if one.contains(name_2):
 #         duplicates.append(name_2)
 
-# end_time = time.time()
-# print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-# print(f"runtime: {end_time - start_time} seconds")
+end_time = time.time()
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish with no restrictions on techniques or data
 # structures?
-
-for one, two in itertools.combinations(names_1, names_2):
-    if one == two:
-        duplicates.append(one)
-print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print(f"runtime: {end_time - start_time} seconds")
